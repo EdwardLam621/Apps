cc6fde27a153c2b94728d8bd2664b975922c7035
Hou Teng Edward Lam <lamhoutenged@seattleu.edu>
2020-11-01 14:42:00 -0800

    Updated Point method

--- 16-Stub-BezierCurve.cpp
+++ 16-Stub-BezierCurve.cpp
@@ -1,4 +1,5 @@
 // BezierCurve.cpp - interactive curve design
+// 16.8 et Maintenant, Bezier by Edward Lam
 
 #include <glad.h>
 #include <GLFW/glfw3.h>
@@ -22,23 +23,59 @@ public:
     Bezier(const vec3 &p1, const vec3 &p2, const vec3 &p3, const vec3 &p4, int res = 50) :
         p1(p1), p2(p2), p3(p3), p4(p4), res(res) { }
     vec3 Point(float t) {
-        // return a point on the Bezier curve given parameter t, in (0,1)
+        float t_sqrd = (t * t);
+        float t_cubed = (t * t * t);
+        vec3 first_t = p1 * (-t_cubed + (3 * t_sqrd) - (3 * t) + 1);
+        vec3 second_t = p2 * ((3 * t_cubed) - (6 * t_sqrd) + (3 * t));
+        vec3 third_t = p3 * ((-3 * t_cubed) + (3 * t_sqrd));
+        vec3 fourth_t = p4 * (t_cubed);
+        return first_t + second_t + third_t + fourth_t;	// point on curve at t
     }
     void Draw(vec3 color, float width) {
         // break the curve into res number of straight pieces
         // *** render each piece with Line() ***
+        vec3 pts[] = { p1, p1 };
+        for (int i = 1; i <= res; i++) {
+            pts[1] = Point((float)i / res);
+            Line(pts[0], pts[1], width, color);
+            pts[0] = pts[1];
+        }
     }
     void DrawControlMesh(vec3 pointColor, vec3 meshColor, float opacity, float width) {
         // draw the four control points and the mesh that connects them
+        UseDrawShader(camera.fullview);
+        Line(p1, p2, 4.0f, meshColor, width, opacity);
+        Line(p2, p3, 4.0f, meshColor, width, opacity);
+        Line(p3, p4, 4.0f, meshColor, width, opacity);
+
+        // round endpoints
+        Disk(p1, 10.0f, pointColor, opacity);
+        Disk(p2, 10.0f, pointColor, opacity);
+        Disk(p3, 10.0f, pointColor, opacity);
+        Disk(p4, 10.0f, pointColor, opacity);
     }
     vec3 *PickPoint(int x, int y, mat4 view) {
         // return pointer to nearest control point, if within 10 pixels of mouse (x,y), else NULL
         // hint: use ScreenDistSq
-        return NULL;
+        float p1_Distance = ScreenDistSq(x, y, p1, view);
+        float p2_Distance = ScreenDistSq(x, y, p2, view);
+        float p3_Distance = ScreenDistSq(x, y, p3, view);
+        float p4_Distance = ScreenDistSq(x, y, p4, view);
+
+        if (p1_Distance < 10)
+            return &p1;
+        else if (p2_Distance < 10)
+            return &p2;
+        else if (p3_Distance < 10)
+            return &p3;
+        else if (p4_Distance < 10)
+            return &p4;
+        else
+            return NULL;
     }
 };
 
-Bezier  curve();//vec3(???), vec3(???), vec3(???), vec3(???));
+Bezier  curve(vec3(.1, .3, 0), vec3(.3, .4, .2), vec3(-.2, .3, .2), vec3(-.3, .1, .2)) ;//vec3(???), vec3(???), vec3(???), vec3(???));
 
 // Display
 
@@ -114,7 +151,7 @@ void Resize(GLFWwindow *w, int width, int height) {
 int main(int ac, char **av) {
     // init app window and GL context
     glfwInit();
-    GLFWwindow *w = glfwCreateWindow(winW, winH, "Bezier Curve", NULL, NULL);
+    GLFWwindow *w = glfwCreateWindow(winW, winH, "Bezier Curve by Edward Lam", NULL, NULL);
     glfwSetWindowPos(w, 100, 100);
     glfwMakeContextCurrent(w);
     gladLoadGLLoader((GLADloadproc) glfwGetProcAddress);
